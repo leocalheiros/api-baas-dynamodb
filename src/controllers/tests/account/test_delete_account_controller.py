aff62e679327ce124b2895eea9d74926a938bd74
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-16 10:02:05 -0300

    fix: refactoring controllers tests to works with new jwt logic

--- src/controllers/tests/account/test_delete_account_controller.py
+++ src/controllers/tests/account/test_delete_account_controller.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 import pytest
 from src.controllers.account.delete_account_controller import DeleteAccountController
+from src.errors.types.http_unauthorized import HttpUnauthorizedError
 from src.errors.types.http_not_found import HttpNotFoundError
 
 
@@ -16,7 +17,7 @@ def test_delete_account_success(delete_account_controller):
         "email": "test@example.com"
     }
 
-    response = delete_account_controller.operate(account_data)
+    response = delete_account_controller.operate(account_data, "test@example.com")
 
     delete_account_controller._DeleteAccountController__model.delete_account.assert_called_once_with(
         "test@example.com"
@@ -25,14 +26,24 @@ def test_delete_account_success(delete_account_controller):
     assert response == "Conta deletada com sucesso!"
 
 
-def test_delete_account_nonexistent_email(delete_account_controller):
+def test_delete_account_with_no_existent_account(delete_account_controller):
+    account_data = {
+        "email": "test@example.com"
+    }
+    delete_account_controller._DeleteAccountController__model.check_account_exists.return_value = False
+
+    with pytest.raises(HttpNotFoundError) as exc_info:
+        delete_account_controller.operate(account_data, "test@example.com")
+
+
+def test_delete_account_with_no_jwt_email_in_headers(delete_account_controller):
     account_data = {
         "email": "nonexistent@example.com"
     }
 
     delete_account_controller._DeleteAccountController__model.check_account_exists.return_value = False
 
-    with pytest.raises(HttpNotFoundError) as exc_info:
-        delete_account_controller.operate(account_data)
+    with pytest.raises(HttpUnauthorizedError) as exc_info:
+        delete_account_controller.operate(account_data, "test@example.com")
 
-    assert str(exc_info.value) == "Email não existente no banco de dados!"
+    assert str(exc_info.value) == "Email na solicitação não corresponde ao email nos cabeçalhos!"
--- src/controllers/tests/account/test_get_account_by_email.py
+++ src/controllers/tests/account/test_get_account_by_email.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock
 import pytest
 from src.controllers.account.get_account_by_email_controller import GetAccountByEmailController
 from src.errors.types.http_not_found import HttpNotFoundError
+from src.errors.types.http_unauthorized import HttpUnauthorizedError
 
 
 @pytest.fixture
@@ -18,14 +19,13 @@ def get_account_by_email_controller():
 def test_get_account_by_email_success(get_account_by_email_controller):
     email = "test@example.com"
 
-    response = get_account_by_email_controller.operate(email)
+    response = get_account_by_email_controller.operate({"email": email}, email)
 
     get_account_by_email_controller._GetAccountByEmailController__model.get_account_by_email.assert_called_once_with(
         email
     )
 
-    assert response in ['Conta encontrada: {"email": "test@example.com", "saldo": 100}',
-                        'Conta encontrada: {\'email\': \'test@example.com\', \'saldo\': 100}']
+    assert 'Conta encontrada' in response
 
 
 def test_get_account_by_email_nonexistent_email(get_account_by_email_controller):
@@ -34,6 +34,15 @@ def test_get_account_by_email_nonexistent_email(get_account_by_email_controller)
     get_account_by_email_controller._GetAccountByEmailController__model.check_account_exists.return_value = False
 
     with pytest.raises(HttpNotFoundError) as exc_info:
-        get_account_by_email_controller.operate(email)
+        get_account_by_email_controller.operate({"email": email}, email)
 
     assert str(exc_info.value) == "Conta não existente no banco de dados"
+
+
+def test_get_account_by_email_unauthorized(get_account_by_email_controller):
+    email = "test@example.com"
+
+    with pytest.raises(HttpUnauthorizedError) as exc_info:
+        get_account_by_email_controller.operate({"email": "unauthorized@example.com"}, email)
+
+    assert str(exc_info.value) == "Email na solicitação não corresponde ao email nos cabeçalhos!"
