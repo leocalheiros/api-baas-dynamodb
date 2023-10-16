5a20a12420afef3e65c5884ed89ce7e8044f2644
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-16 09:41:46 -0300

    fix: refactoring delete_account_controller to works with jwt email in headers

--- src/controllers/account/delete_account_controller.py
+++ src/controllers/account/delete_account_controller.py
@@ -1,18 +1,21 @@
 from src.models.interface.account_interface import AccountRepositoryInterface
-from src.controllers.interface.controller_interface import ControllerInterface
+from src.controllers.interface.account_controller_interface import ControllerInterface
 from src.errors.types.http_not_found import HttpNotFoundError
+from src.errors.types.http_unauthorized import HttpUnauthorizedError
 
 
 class DeleteAccountController(ControllerInterface):
     def __init__(self, model: AccountRepositoryInterface):
         self.__model = model
 
-    def operate(self, account: dict) -> str:
+    def operate(self, account: dict, request_email: str) -> str:
         email = account.get("email")
-        self.__validate(email)
+        self.__validate(email, request_email)
         self.__model.delete_account(email)
         return "Conta deletada com sucesso!"
 
-    def __validate(self, email: str) -> None:
+    def __validate(self, email: str, request_email: str) -> None:
+        if email != request_email:
+            raise HttpUnauthorizedError("Email na solicitação não corresponde ao email nos cabeçalhos!")
         if not self.__model.check_account_exists(email):
             raise HttpNotFoundError("Email não existente no banco de dados!")
