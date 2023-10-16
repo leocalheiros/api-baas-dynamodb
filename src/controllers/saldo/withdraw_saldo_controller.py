c00e089cadc57e2e212b34c021c19a755bc2b223
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-16 09:43:57 -0300

    fix: refactoring withdraw_saldo_controller to works with jwt email in headers

--- src/controllers/saldo/withdraw_saldo_controller.py
+++ src/controllers/saldo/withdraw_saldo_controller.py
@@ -3,6 +3,7 @@ from src.models.interface.account_interface import AccountRepositoryInterface
 from src.controllers.interface.saldo_controller_interface import SaldoControllerInterface
 from src.errors.types.http_not_found import HttpNotFoundError
 from src.errors.types.http_insufficient_balance_error import HttpInsufficientBalanceError
+from src.errors.types.http_unauthorized import HttpUnauthorizedError
 
 
 class WithdrawSaldoController(SaldoControllerInterface):
@@ -10,12 +11,16 @@ class WithdrawSaldoController(SaldoControllerInterface):
         self.__account_model = account_model
         self.__saldo_model = saldo_model
 
-    def operate(self, email: str, amount: int) -> str:
-        self.__validate(email, amount)
+    def operate(self, request_data: dict, request_email: str) -> str:
+        email = request_data.get("email")
+        amount = request_data.get("amount")
+        self.__validate(email, amount, request_email)
         self.__saldo_model.add_saldo(email, -amount)
         return f"Saque de {amount} realizado com sucesso na conta de {email}"
 
-    def __validate(self, email: str, amount: int) -> None:
+    def __validate(self, email: str, amount: int, request_email: str) -> None:
+        if email != request_email:
+            raise HttpUnauthorizedError("Email na solicitação não corresponde ao email nos cabeçalhos!")
         if not self.__account_model.check_account_exists(email):
             raise HttpNotFoundError("Conta não existente no banco de dados")
         if not self.__check_saldo(email, amount):
