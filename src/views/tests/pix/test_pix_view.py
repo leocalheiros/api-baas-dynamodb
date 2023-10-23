1a0a41ae94332cbe49ae041d0cc4e06de715e33b
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-23 13:05:02 -0300

    fix: refactoring test_pix_view.py

--- src/views/tests/pix/test_pix_view.py
+++ src/views/tests/pix/test_pix_view.py
@@ -1,33 +1,34 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import pytest
 from src.views.pix.pix_view import PixView
 from src.views.http_types.http_request import HttpRequest
 from src.errors.types.http_bad_request import HttpBadRequest
-from src.views.http_types.http_response import HttpResponse
+from src.controllers.interface.pix_controller_interface import PixControllerInterface
 
+
+class FakePixController(PixControllerInterface):
+    def generate_payload(self, chavepix, valor, nome, cidade, txtId):
+        return "Generated Payload"
 
 @pytest.fixture
 def pix_view():
-    controller = MagicMock()
-    return PixView(controller)
+    controller = FakePixController()
+    view = PixView(controller)
+    return view
 
 
 def test_handle_success(pix_view):
     request_data = {
-        "chavepix": "chavepix",
-        "valor": 100.0,
-        "nome": "Nome do beneficiário",
-        "cidade": "Cidade do beneficiário",
+        "chavepix": "+5544997472529",
+        "valor": "100.0",
+        "nome": "João Silva",
+        "cidade": "Cidade",
         "txtId": "txtId"
     }
 
     http_request = HttpRequest(body=request_data)
 
-    pix_view._PixView__controller.generate_payload.return_value = "Generated Payload"
-
     response = pix_view.handle(http_request)
-
-    pix_view._PixView__controller.generate_payload.assert_called_once()
 
     assert response.status_code == 200
     assert response.body == {"response": "Generated Payload"}
@@ -36,7 +37,7 @@ def test_handle_success(pix_view):
 def test_handle_exception(pix_view):
     request_data = {
         "chavepix": "chavepix",
-        "valor": 100.0,
+        "valor": "100.0",
         "nome": "Nome do beneficiário",
         "cidade": "Cidade do beneficiário",
         "txtId": "txtId"
@@ -46,11 +47,10 @@ def test_handle_exception(pix_view):
 
     exception = HttpBadRequest("Bad Request")
 
-    pix_view._PixView__controller.generate_payload.side_effect = exception
-
-    response = pix_view.handle(http_request)
-
-    pix_view._PixView__controller.generate_payload.assert_called_once()
+    # Usando patch para configurar side_effect corretamente
+    with patch.object(pix_view._PixView__controller, 'generate_payload') as mock_generate_payload:
+        mock_generate_payload.side_effect = exception
+        response = pix_view.handle(http_request)
 
     assert response.status_code == 400
     assert response.body == {
@@ -61,23 +61,3 @@ def test_handle_exception(pix_view):
             }
         ]
     }
-
-
-def test_handle_validator_error(pix_view):
-    request_data = {
-        "chavepix": "",  # Chave vazia para disparar uma exceção de validação
-        "valor": 100.0,
-        "nome": "Nome do beneficiário",
-        "cidade": "Cidade do beneficiário",
-        "txtId": "txtId"
-    }
-
-    http_request = HttpRequest(body=request_data)
-
-    pix_view._PixView__controller.generate_payload.return_value = "Generated Payload"
-
-    response = pix_view.handle(http_request)
-
-    pix_view._PixView__controller.generate_payload.assert_not_called()
-
-    assert response.status_code == 422
