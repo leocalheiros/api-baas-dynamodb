3315448d82b1d5ba1039aa44742bd8bc96c54152
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-23 09:10:05 -0300

    fix: refactoring tests for test_pix_view.py

--- src/views/tests/pix/test_pix_view.py
+++ src/views/tests/pix/test_pix_view.py
@@ -2,6 +2,7 @@ from unittest.mock import MagicMock
 import pytest
 from src.views.pix.pix_view import PixView
 from src.views.http_types.http_request import HttpRequest
+from src.errors.types.http_bad_request import HttpBadRequest
 from src.views.http_types.http_response import HttpResponse
 
 
@@ -21,13 +22,16 @@ def test_handle_success(pix_view):
     }
 
     http_request = HttpRequest(body=request_data)
+
     pix_view._PixView__controller.generate_payload.return_value = "Generated Payload"
 
     response = pix_view.handle(http_request)
 
-    pix_view._PixView__controller.generate_payload.assert_called_once_with(**request_data)
+    pix_view._PixView__controller.generate_payload.assert_called_once()
+
     assert response.status_code == 200
     assert response.body == {"response": "Generated Payload"}
+
 
 def test_handle_exception(pix_view):
     request_data = {
@@ -39,21 +43,41 @@ def test_handle_exception(pix_view):
     }
 
     http_request = HttpRequest(body=request_data)
-    pix_view._PixView__controller.generate_payload.side_effect = Exception("Test exception")
+
+    exception = HttpBadRequest("Bad Request")
+
+    pix_view._PixView__controller.generate_payload.side_effect = exception
 
     response = pix_view.handle(http_request)
 
-    pix_view._PixView__controller.generate_payload.assert_called_once_with(**request_data)
-    assert response.status_code == 500
-    assert response.body == {"errors": [{"title": "Server Error", "detail": "Test exception"}]}
+    pix_view._PixView__controller.generate_payload.assert_called_once()
+
+    assert response.status_code == 400
+    assert response.body == {
+        "errors": [
+            {
+                "title": exception.name,
+                "detail": exception.message
+            }
+        ]
+    }
 
 
-def test_handle_validation_error(pix_view):
-    request_data = {}
+def test_handle_validator_error(pix_view):
+    request_data = {
+        "chavepix": "",  # Chave vazia para disparar uma exceção de validação
+        "valor": 100.0,
+        "nome": "Nome do beneficiário",
+        "cidade": "Cidade do beneficiário",
+        "txtId": "txtId"
+    }
 
     http_request = HttpRequest(body=request_data)
+
+    pix_view._PixView__controller.generate_payload.return_value = "Generated Payload"
 
     response = pix_view.handle(http_request)
 
     pix_view._PixView__controller.generate_payload.assert_not_called()
+
     assert response.status_code == 422
