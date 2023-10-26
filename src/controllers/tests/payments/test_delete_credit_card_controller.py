646efdb85f23f9c57c7749e123d6ec9653780ab0
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-10-26 11:50:31 -0300

    feat: add delete_credit_card_controller.py to controllers

---
+++ src/controllers/tests/payments/test_delete_credit_card_controller.py
@@ -0,0 +1,73 @@
+import pytest
+from unittest.mock import Mock
+from src.controllers.payments.create_credit_card_payment_controller import TransactionController
+from src.models.interface.account_interface import AccountRepositoryInterface
+from src.models.interface.credit_card_interface import CreditCardRepositoryInterface
+from src.errors.types.http_not_found import HttpNotFoundError
+
+
+def create_account_model_mock(exists=True):
+    account_model_mock = Mock(spec=AccountRepositoryInterface)
+    account_model_mock.check_account_exists.return_value = exists
+    return account_model_mock
+
+
+def create_card_model_mock(has_credit_card=True):
+    card_model_mock = Mock(spec=CreditCardRepositoryInterface)
+    if has_credit_card:
+        card_model_mock.get_credit_card.return_value = {
+            "card_number": "NDEzNDE4NTc3OTk5NTAwMA=="
+        }
+    else:
+        card_model_mock.get_credit_card.return_value = None
+    return card_model_mock
+
+
+def test_transaction_controller_success():
+    account_model_mock = create_account_model_mock(exists=True)
+    card_model_mock = create_card_model_mock(has_credit_card=True)
+    controller = TransactionController(account_model_mock, card_model_mock)
+
+    request_data = {
+        'email': 'user@example.com',
+    }
+
+    response = controller.operate(request_data, 'user@example.com')
+
+    expected_response = {
+        "data": {
+            "status": "success",
+            "email": 'user@example.com',
+            "card_number": "4134185779995000"
+        }
+    }
+
+    assert response == expected_response
+
+
+def test_transaction_controller_account_not_found():
+    account_model_mock = create_account_model_mock(exists=False)
+    card_model_mock = create_card_model_mock(has_credit_card=True)
+    controller = TransactionController(account_model_mock, card_model_mock)
+
+    request_data = {
+        'email': 'user@example.com',
+        'amount': 100,
+    }
+
+    with pytest.raises(HttpNotFoundError):
+        controller.operate(request_data, 'user@example.com')
+
+
+def test_transaction_controller_no_credit_card():
+    account_model_mock = create_account_model_mock(exists=True)
+    card_model_mock = create_card_model_mock(has_credit_card=False)
+    controller = TransactionController(account_model_mock, card_model_mock)
+
+    request_data = {
+        'email': 'user@example.com',
+        'amount': 100,
+    }
+
+    with pytest.raises(HttpNotFoundError):
+        controller.operate(request_data, 'user@example.com')
