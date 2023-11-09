badb701e2ee48e2e286cd66dd94ef849fdbdad5b
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-09 15:57:50 -0300

    feat: add Payments blueprint in lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,10 +2,12 @@ import awsgi
 from flask import Flask
 from src.main.routes import bank_blueprint
 from src.main.routes import saldo_blueprint
+from src.main.routes import payments_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(bank_blueprint)
 app.register_blueprint(saldo_blueprint)
+app.register_blueprint(payments_blueprint)
 
 
 def lambda_handler(event, context):
