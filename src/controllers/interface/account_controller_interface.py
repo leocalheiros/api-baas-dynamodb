229f165d130642ed3092ee88fa88031d767b83e5
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-29 08:08:20 -0300

    fix: refactoring account controller interface

--- src/controllers/interface/account_controller_interface.py
+++ src/controllers/interface/account_controller_interface.py
@@ -4,5 +4,5 @@ from abc import ABC, abstractmethod
 class ControllerInterface(ABC):
 
     @abstractmethod
-    def operate(self, account: dict) -> any:
+    def operate(self, account: dict, request_email: str) -> any:
         pass
